64e9ee19418a5251d1525ed5217f6c6633d79e33
remitruong <remi.truong98@gmail.com>
2020-11-13 18:34:12 +0100

    correction

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get('/*', function (req, res) {
   );
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
